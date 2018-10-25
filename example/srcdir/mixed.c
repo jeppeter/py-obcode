b531cb70c529d82540f8121ac410e04e0db9ca29
jeppeter <jeppeter@gmail.com>
2018-10-25 13:44:06 +0800

    to coding for the OB_MIXED_STR OB_MIXED_STR_SPEC OB_MIXED_WSTR OB_MIXED_WSTR_SPEC

---
+++ example/srcdir/mixed.c
@@ -0,0 +1,60 @@
+#include <obcode.h>
+#include <stdio.h>
+#include <wchar.h>
+
+OB_CONFIG("maxround=10,funcmax=1,funcmin=1,namemax=10,namemin=5");
+OB_INSERT();
+
+int CallNew()
+{
+	int a,b,c;
+	a = 1;
+	b = 2;
+	c = 3;
+	printf(OB_STR_MIXED("[%s:%d] up hello world %d %d %d\n"),__FILE__,__LINE__,a,b,c);
+	return 0;
+}
+
+extern int OB_VAR(newvar2);
+extern int OB_VAR_SPEC("namemax=100",newvar5);
+int OB_DECL_VAR(newvar)=2;
+int OB_DECL_VAR_SPEC("namemax=20",newvv)=20;
+
+int OB_FUNC PrintFunc()
+{
+	int a,b,c;
+	a = 1;
+	b = 2;
+	c = 3;
+	OB_CODE(a,b,c);
+	printf(OB_STR_MIXED_SPEC("funcname=17","[%s:%d] hello world %d %d %d\n"),__FILE__,__LINE__,a,b,c);
+	OB_CODE_SPEC("funcmax=3,funcmin=1,debug=10",a,b,c);
+	printf(OB_STR_MIXED_SPEC("funcmin=10,funcmax=30","[%s:%d] hello world again %d %d %d\n"),__FILE__,__LINE__,a,b,c);
+	fwprintf(stderr,OB_WSTR_MIXED(L"[%hs:%d] hello world wide %d %d %d\n"),__FILE__,__LINE__,a,b,c);
+	fwprintf(stderr,OB_WSTR_MIXED_SPEC("funcname=30",L"[%hs:%d] hello world wide %d %d %d\n"),__FILE__,__LINE__,a,b,c);
+	CallNew();
+	return 0;
+}
+
+int OB_FUNC_SPEC("namemin=20,namemax=30") PrintFunc2()
+{
+	int a,b,c;
+	a = 1;
+	b = 2;
+	c = 3;
+	OB_CODE_SPEC("funcmin=20,funcmax=30",a,b,c);
+	printf("[%s:%d] hello world %d %d %d\n",__FILE__,__LINE__,a,b,c);
+	OB_CODE_SPEC("funcmax=3,funcmin=1,debug=10",a,b,c);
+	printf("[%s:%d] hello world again %d %d %d\n",__FILE__,__LINE__,a,b,c);
+	CallNew();
+	return 0;	
+}
+
+int main(int argc,char* argv[])
+{
+	newvar = 0;
+	argc = argc;
+	argv = argv;
+	PrintFunc();
+	return 0;
+}
