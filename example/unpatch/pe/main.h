a4ef8eb4fecdb86386c14bdc75b19d05cabb5498
jeppeter <jeppeter@gmail.com>
2018-11-16 11:39:48 +0800

    to compile ok for unpatch pe

---
+++ example/unpatch/pe/main.h
@@ -0,0 +1,20 @@
+#ifndef __MAIN_H_B4A752BC7B8694C38FD413662A4302B4__
+#define __MAIN_H_B4A752BC7B8694C38FD413662A4302B4__
+
+#include <obcode.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int OB_FUNC unpatch_handler(map_prot_func_t protfunc);
+int print_out_a(void);
+int print_out_b(void);
+int print_out_c(void);
+
+#ifdef __cplusplus
+};
+#endif
+
+
+#endif /* __MAIN_H_B4A752BC7B8694C38FD413662A4302B4__ */
