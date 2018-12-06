5cce3813d611c3a93db97dc3b6e264a142864db6
jeppeter <jeppeter@gmail.com>
2018-12-06 15:11:19 +0800

    to add crc32calc.c

--- test/chkintegrity/main.cpp
+++ test/chkintegrity/main.cpp
@@ -59,36 +59,7 @@ int init_log_verbose(pargs_options_t pargs)
     return 0;
 }
 
-int crc32_calc(unsigned char *message,unsigned int size, unsigned char* pval, int valsize)
-{
-    unsigned int i;
-    int j;
-    unsigned int byte, crc, mask;
-
-    i = 0;
-    crc = 0xFFFFFFFF;
-    while (i < size) {
-        byte = message[i];            // Get next byte.
-        crc = crc ^ byte;
-        for (j = 7; j >= 0; j--) {    // Do eight times.
-            mask = 0;
-            if (crc & 1) {
-                mask = 0xffffffff;
-            }
-            //mask = - (crc & 1);
-            crc = (crc >> 1) ^ (0xEDB88320 & mask);
-        }
-        i = i + 1;
-    }
-    crc = ~crc;
-    if (pval && valsize >= 4) {
-        for (i=0;i<4;i++) {
-            pval[i] = ((crc >> (i * 8)) & 0xff);
-        }
-    }
-    return 4;
-}
-
+#include "crc32calc.c"
 
 int crc32_handler(int argc, char* argv[], pextargs_state_t parsestate, void* popt)
 {
