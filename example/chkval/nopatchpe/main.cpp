7718ba6687d5fa668d129a2ee7b451eaccdc1820
jeppeter <jeppeter@gmail.com>
2019-01-17 11:33:25 +0800

    to make chkval/nopatchpe with end of space in cc

--- example/chkval/nopatchpe/main.cpp
+++ example/chkval/nopatchpe/main.cpp
@@ -1,31 +1,30 @@
-#include "main2.h"
+#include "main.h"
 #include "callc.h"
-#include "chk_main2.h"
+#include "chk_main.h"
 #include "handle_exit.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 OB_INSERT();
 
-#define OUTP(...) do{fprintf(stdout,"[%s:%d] ",__FILE__,__LINE__); fprintf(stdout,__VA_ARGS__);fprintf(stdout,"\n");}while(0)
+#define OUTP(...) do{fprintf(stdout,"[%s:%d] ",C_FILE,__LINE__); fprintf(stdout,__VA_ARGS__);fprintf(stdout,"\n");}while(0)
 
 #define SVAL           0x11223344
 #define RVAL           0x55667788
 
-
-void failfunc_main2_1(int exitcode, char* name)
+void failfunc_main_1(int exitcode, char* name)
 {
 	OUTP("fail [%d] [%s]", exitcode, name);
 	exit(exitcode);
 }
 
 
-int print_out2_a(void)
+int print_out_a(void)
 {
 	int a=1,b=2,c=SVAL;
 	int ret;
 	OB_CODE(a,b,c);
-	ret = OB_CHKVAL_FUNC(failfunc_main2_1);
+	ret = OB_CHKVAL_FUNC(failfunc_main_1);
 	if (ret < 0) {
 		return ret;
 	}
@@ -35,7 +34,7 @@ int print_out2_a(void)
 	return 0;
 }
 
-int print_out2_b(void)
+int print_out_b(void)
 {
 	int a=1,b=2,c=SVAL;
 	int ret;
@@ -50,7 +49,7 @@ int print_out2_b(void)
 	return 0;
 }
 
-int print_out2_c(void)
+int print_out_c(void)
 {
 	int a=1,b=2,c=SVAL;
 	int ret;
@@ -65,63 +64,64 @@ int print_out2_c(void)
 	return 0;
 }
 
-int print_out2_d(void)
+
+int print_out_d(void)
 {
-	int a=1,b=2,c=3;
+	int a=1,b=2,c=SVAL;
 	int ret;
 	OB_CODE(a,b,c);
 	ret = OB_CHKVAL_TOTAL_FUNC();
 	if (ret < 0) {
 		return ret;
-	}	
-	OUTP("a=%d;b=%d;c=%d;",a,b,c);
-	OUTP("a=%d;b=%d;c=%d;",a,b,c);
-	OUTP("a=%d;b=%d;c=%d;",a,b,c);
+	}
+	OUTP("a=%d;b=%d;c=0x%x;",a,b,c);
+	OUTP("a=%d;b=%d;c=0x%x;",a,b,c);
+	OUTP("a=%d;b=%d;c=0x%x;",a,b,c);
 	return 0;
 }
 
-int print_out2_e(void)
+int print_out_e(void)
 {
-	int a=1,b=2,c=3;
+	int a=1,b=2,c=SVAL;
 	int ret;
 	OB_CODE(a,b,c);
 	ret = OB_CHKVAL_TOTAL_FUNC();
 	if (ret < 0) {
 		return ret;
-	}	
-	OUTP("a=%d;b=%d;c=%d;",a,b,c);
-	OUTP("a=%d;b=%d;c=%d;",a,b,c);
-	OUTP("a=%d;b=%d;c=%d;",a,b,c);
+	}
+	OUTP("a=%d;b=%d;c=0x%x;",a,b,c);
+	OUTP("a=%d;b=%d;c=0x%x;",a,b,c);
+	OUTP("a=%d;b=%d;c=0x%x;",a,b,c);
 	return 0;
 }
 
-int print_out2_f(void)
+int print_out_f(void)
 {
-	int a=1,b=2,c=3;
+	int a=1,b=2,c=SVAL;
 	int ret;
 	OB_CODE(a,b,c);
 	ret = OB_CHKVAL_TOTAL_FUNC();
 	if (ret < 0) {
 		return ret;
-	}	
-	OUTP("a=%d;b=%d;c=%d;",a,b,c);
-	OUTP("a=%d;b=%d;c=%d;",a,b,c);
-	OUTP("a=%d;b=%d;c=%d;",a,b,c);
+	}
+	OUTP("a=%d;b=%d;c=0x%x;",a,b,c);
+	OUTP("a=%d;b=%d;c=0x%x;",a,b,c);
+	OUTP("a=%d;b=%d;c=0x%x;",a,b,c);
 	return 0;
 }
 
-int print_out2_g(void)
+int print_out_g(void)
 {
-	int a=1,b=2,c=3;
+	int a=1,b=2,c=SVAL;
 	int ret;
 	OB_CODE(a,b,c);
 	ret = OB_CHKVAL_TOTAL_FUNC();
 	if (ret < 0) {
 		return ret;
-	}	
-	OUTP("a=%d;b=%d;c=%d;",a,b,c);
-	OUTP("a=%d;b=%d;c=%d;",a,b,c);
-	OUTP("a=%d;b=%d;c=%d;",a,b,c);
+	}
+	OUTP("a=%d;b=%d;c=0x%x;",a,b,c);
+	OUTP("a=%d;b=%d;c=0x%x;",a,b,c);
+	OUTP("a=%d;b=%d;c=0x%x;",a,b,c);
 	return 0;
 }
 
@@ -177,15 +177,15 @@ unsigned char* get_func_address(unsigned char* ptr)
 int change_values()
 {
 	int ret = 0;
-	ret = change_value(get_func_address((unsigned char*)&print_out2_a), (int)(get_func_address((unsigned char*)&print_out2_b) - get_func_address((unsigned char*)&print_out2_a)), SVAL ,RVAL);
+	ret = change_value(get_func_address((unsigned char*)&print_out_a), (int)(get_func_address((unsigned char*)&print_out_b) - get_func_address((unsigned char*)&print_out_a)), SVAL ,RVAL);
 	if (ret < 0) {
 		return ret;
 	}
-	ret = change_value(get_func_address((unsigned char*)&print_out2_b), (int)(get_func_address((unsigned char*)&print_out2_c) - get_func_address((unsigned char*)&print_out2_b)), SVAL ,RVAL);
+	ret = change_value(get_func_address((unsigned char*)&print_out_b), (int)(get_func_address((unsigned char*)&print_out_c) - get_func_address((unsigned char*)&print_out_b)), SVAL ,RVAL);
 	if (ret < 0) {
 		return ret;
 	}
-	ret = change_value(get_func_address((unsigned char*)&print_out2_c), (int)(get_func_address((unsigned char*)&print_out2_d) - get_func_address((unsigned char*)&print_out2_c)), SVAL ,RVAL);
+	ret = change_value(get_func_address((unsigned char*)&print_out_c), (int)(get_func_address((unsigned char*)&print_out_d) - get_func_address((unsigned char*)&print_out_c)), SVAL ,RVAL);
 	if (ret < 0) {
 		return ret;
 	}
@@ -205,13 +205,13 @@ int main(int argc,char* argv[])
 	if (ret < 0) {
 		return ret;
 	}
-	print_out2_a();
-	print_out2_b();
-	print_out2_c();
-	print_out2_d();
-	print_out2_e();
-	print_out2_f();
-	print_out2_g();
+	print_out_a();
+	print_out_b();
+	print_out_c();
+	print_out_d();
+	print_out_e();
+	print_out_f();
+	print_out_g();
 	callc_a();
 	callc_b();
 	callc_c();
